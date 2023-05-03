7d412e2b8dae375028c02110683877e3ddf29a45
darasn <darasnunes@gmail.com>
2023-05-03 09:12:07 -0300

    fazer teste

--- src/index.js
+++ src/index.js
@@ -1,112 +1,115 @@
-import luhnCheck from './validator.js';
+import { isValid, maskify } from './validator.js';
 
 const myInput = document.getElementById("numero");
-let valueInput = "";
-let security = "";
-let minhaLista = [];
-let lastNumbers = "";
-let firstNumbers = "";
-let visa = document.getElementById("visa");
-let master = document.getElementById("master");
-let elo = document.getElementById("elo");
-let american = document.getElementById("american");
-let hypercard = document.getElementById("hypercard");
+const card = document.getElementById("cartão");
+const message = document.getElementById("message");
+const btnReload = document.getElementById('btnReload');
+const visa = document.getElementById("visa");
+const master = document.getElementById("master");
+const elo = document.getElementById("elo");
+const american = document.getElementById("american");
+const hipercard = document.getElementById("hipercard");
 
-myInput.addEventListener("input", function (event) {
-    security = this.value.replace(/\s/g, ''); // remove espaços em branco
-    minhaLista.push(security);
-    for (let i = 0; i < minhaLista.length; i++) {
-        const ultimoNumero = minhaLista[i].toString().slice(-1);
-        valueInput += ultimoNumero;
-    }
-    const lastFourDigits = security.slice(-4); // pega os últimos quatro dígitos
-    const maskedDigits = security.slice(0, -4).replace(/\d/g, "*"); // substitui os demais dígitos por "*"
-    const maskedNumber = maskedDigits + lastFourDigits;
-    this.value = maskedNumber;
-    lastNumbers = valueInput.toString().slice(-16);    
-    
-});
+let teste = "";
+let continuar = true;
+let cardNameFlag = "";
 
 const flagCodes = {
+    //lista de códigos das bandeiras utilizada parta trocar a bandeira do cartão quando o usuário digita
     visa: [41, 42, 43, 44, 45, 46, 47, 48, 49],
     master1: [51, 52, 53, 54, 55],
     master2: [22, 23, 24, 25, 26, 27],
-    elo:[40, 50, 65],
+    elo: [40, 50, 65],
     american: [34, 37],
     Hipercard: [38, 60]
 };
 
-let continuar = true;
-let cardNameFlag;
+myInput.addEventListener("input", function (event) {
+    //Parte do código que mascara os digitos que o usuario coloca no input
+    let lastNumbers = maskify(this);
+    teste += lastNumbers;
+});
 
-//keydown serve para guardar o valor quando o enter for apertado.
+
 myInput.addEventListener("keydown", function (event) {
-    let firstNumbers = lastNumbers.slice(0, 2);
+    //keydown serve para guardar o valor quando o enter for apertado.
+    console.log('este é o teste com type ' + typeof teste);
+    let firstNumbers = teste.slice(0, 2);
+    let intNumber = parseInt(firstNumbers); // => precisei passar para inteiro por meio de outra variável. Ainda nãaei o porque que não funcionou usando parse direto no if.
     if (event.keyCode === 13) {
-        if (!security || /[^0-9*]+/.test(security)) {
+        //Se apertar a tecla enter
+        if (!teste || /[^0-9*]+/.test(teste)) {
+            //Verifica se foi digitado apenas números e retorna pedindo para reescrever se houver algo diferernte de "0123456789".
             alert('Digite apenas numeros');
         } else {
-            for (let chave in flagCodes){
-                if (continuar){
-                    for (let iten in flagCodes[chave]){
-                    console.log(typeof flagCodes[chave][iten], flagCodes[chave][iten])
-                    console.log(typeof firstNumbers, firstNumbers)
-                    if(parseInt(firstNumbers) === flagCodes[chave][iten]){
-                        cardNameFlag = chave;
-                        console.log("achei " + chave);
-                        console.log("achei agora " + cardNameFlag);
-                        console.log("esse deveria ser o " + firstNumbers);
-                        continuar = false;
-                        break;
+            for (let chave in flagCodes) {
+                if (continuar) {
+                    for (let iten in flagCodes[chave]) {
+                        if (intNumber === flagCodes[chave][iten]) {
+                            cardNameFlag = chave;                                   //console.log("achei " + chave);                       
+                            continuar = false;                                      //console.log("achei agora " + cardNameFlag);
+                            if (cardNameFlag === "visa") {                          //console.log("esse deveria ser o " + firstNumbers);                              
+                                //console.log("card visa")                          //console.log(typeof cardNameFlag)
+                                visa.style.display = "inline";
+                                master.style.display = "none";
+                                elo.style.display = "none";
+                                american.style.display = "none";
+                                hipercard.style.display = "none";
+
+                            } else if (cardNameFlag === "master1" || cardNameFlag === "master2") {
+                                //console.log("card master")
+                                visa.style.display = "none";
+                                master.style.display = "inline";
+                                elo.style.display = "none";
+                                american.style.display = "none";
+                                hipercard.style.display = "none";
+
+                            } else if (cardNameFlag === "elo") {
+                                //console.log("card elo")
+                                visa.style.display = "none";
+                                master.style.display = "none";
+                                elo.style.display = "inline";
+                                american.style.display = "none";
+                                hipercard.style.display = "none";
+
+                            } else if (cardNameFlag === "american") {
+                                //console.log("card ame")
+                                visa.style.display = "none";
+                                master.style.display = "none";
+                                elo.style.display = "none";
+                                american.style.display = "inline";
+                                hipercard.style.display = "none";
+
+                            } else if (cardNameFlag === "hipercard") {
+                                //console.log("card hiper")
+                                visa.style.display = "none";
+                                master.style.display = "none";
+                                elo.style.display = "none";
+                                american.style.display = "none";
+                                hipercard.style.display = "inline";
+                            }
+
+                            break;
+
                         }
                     }
-                }	
+                }
             }
-            alert(firstNumbers);
-            alert(cardNameFlag);
-            const isValid = luhnCheck(lastNumbers);
-            if (isValid) {
-                alert('numero de cartão válido');
+            const valid = isValid(teste);
+            if (valid) {
+                card.style.backgroundColor = "#90EE90"
+                message.innerHTML = "CARTÃO VÁLIDO"
+                myInput.disabled = true;
+
             } else {
-                alert('Numero de cartão inválido');
+                card.style.backgroundColor = "#FF6347"
+                message.innerHTML = "CARTÃO INVÁLIDO"
+                myInput.disabled = true;
             }
         }
     }
 });
 
-
-if (cardNameFlag === "visa"){
-    visa.style.display = "inline";
-    master.style.display = "none";
-    elo.style.display = "none";
-    american.style.display = "none";
-    hypercard.style.display = "none";
-
-} else if (cardNameFlag === "master1" ||  cardNameFlag === "master2"){
-    visa.style.display = "none";
-    master.style.display = "inline";
-    elo.style.display = "none";
-    american.style.display = "none";
-    hypercard.style.display = "none";
-
-} else if (cardNameFlag === "elo"){
-    visa.style.display = "none";
-    master.style.display = "none";
-    elo.style.display = "inline";
-    american.style.display = "none";
-    hypercard.style.display = "none";
-
-}else if (cardNameFlag === "american"){
-    visa.style.display = "none";
-    master.style.display = "none";
-    elo.style.display = "none";
-    american.style.display = "inline";
-    hypercard.style.display = "none";
-
-}else if (cardNameFlag === "hypercard"){
-    visa.style.display = "none";
-    master.style.display = "none";
-    elo.style.display = "none";
-    american.style.display = "none";
-    hypercard.style.display = "inline";
-}
+btnReload.addEventListener('click', function () {
+    location.reload();
+});
--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,4 @@
-function luhnCheck(valueInput) {
+export function isValid(valueInput) {
 
   let soma = 0;
   let digitosPares = false;
@@ -16,7 +16,45 @@ function luhnCheck(valueInput) {
   // Verifica se o resultado é divisível por 10.
   return soma % 10 == 0;
 }
-// ...
 
+/*
+export function maskify(Input) {
+  let security = "";
+  let minhaLista = [];
+  let valueInput = "";
+  let lastNumbers = "";
+  Input.addEventListener("input", function (event) {
+    security = this.value.replace(/\s/g, ""); // remove espaços em branco
+    minhaLista.push(security);
+    for (let i = 0; i < minhaLista.length; i++) {
+      const ultimoNumero = minhaLista[i].toString().slice(-1);
+      valueInput += ultimoNumero;
+    }
+    const lastFourDigits = security.slice(-4); // pega os últimos quatro dígitos
+    const maskedDigits = security.slice(0, -4).replace(/\d/g, "*"); // substitui os demais dígitos por "*"
+    const maskedNumber = maskedDigits + lastFourDigits;
+    this.value = maskedNumber;
+    lastNumbers = valueInput.toString().slice(-16);
+    return parseInt(lastNumbers);
+  });
+}
+*/
 
-export default luhnCheck;
+export function maskify(input) {
+  let security = "";
+  let minhaLista = [];
+  let valueInput = "";
+  let lastNumbers = "";
+  security = input.value.replace(/\s/g, ""); // remove espaços em branco
+    minhaLista.push(security);
+    for (let i = 0; i < minhaLista.length; i++) {
+      const ultimoNumero = minhaLista[i].toString().slice(-1);
+      valueInput += ultimoNumero;
+    }
+    const lastFourDigits = security.slice(-4); // pega os últimos quatro dígitos
+    const maskedDigits = security.slice(0, -4).replace(/\d/g, "#"); // substitui os demais dígitos por "*"
+    const maskedNumber = maskedDigits + lastFourDigits;
+    input.value = maskedNumber;
+    lastNumbers = valueInput.toString().slice(-16);
+    return parseInt(lastNumbers);
+}
